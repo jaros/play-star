eddda7ef920b44fd7c5ef778ac4d41c1378a3dd7
jaros <judin.jaroslav@gmail.com>
2017-10-20 00:23:38 +0200

    impl custom cache

--- app/controllers/DataSample.scala
+++ app/controllers/DataSample.scala
@@ -1,31 +1,32 @@
 package controllers
 
+import java.util.concurrent.TimeUnit
+
 import org.json4s._
 import org.json4s.native.JsonMethods._
+
+import scala.collection.mutable
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Deadline, Duration}
+import scala.util.Try
+import scala.xml.Elem
 
 object DataSample {
 
 
+  val cache: mutable.Map[String, Elem] = mutable.Map.empty
+
+
   def main(args: Array[String]): Unit = {
+    loadCachedXml
+    loadCachedXml
+    loadCachedXml
+
+    delay(Duration(7, TimeUnit.SECONDS).fromNow)
+
     val sampleXml =
-      <CATALOG>
-        <CD>
-          <TITLE>Empire Burlesque</TITLE>
-          <ARTIST>Bob Dylan</ARTIST>
-          <COUNTRY>USA</COUNTRY>
-          <COMPANY>Columbia</COMPANY>
-          <PRICE>10.90</PRICE>
-          <YEAR>1985</YEAR>
-        </CD>
-        <CD>
-          <TITLE>Hide your heart</TITLE>
-          <ARTIST>Bonnie Tyler</ARTIST>
-          <COUNTRY>UK</COUNTRY>
-          <COMPANY>CBS Records</COMPANY>
-          <PRICE>9.90</PRICE>
-          <YEAR>1988</YEAR>
-        </CD>
-      </CATALOG>
+      loadCachedXml
 
     println(sampleXml)
     val jValue = Xml.toJson(sampleXml)
@@ -38,4 +39,37 @@ object DataSample {
 
   }
 
+  def loadCachedXml = cache.getOrElseUpdate("myXml", {
+    Future {
+      delay(Duration(5, TimeUnit.SECONDS).fromNow)
+      cache.remove("myXml")
+    }
+    loadXml
+  })
+
+  def delay(dur: Deadline) = {
+    Try(Await.ready(Promise().future, dur.timeLeft))
+  }
+
+  private def loadXml = {
+    println("tet")
+    <CATALOG>
+      <CD>
+        <TITLE>Empire Burlesque</TITLE>
+        <ARTIST>Bob Dylan</ARTIST>
+        <COUNTRY>USA</COUNTRY>
+        <COMPANY>Columbia</COMPANY>
+        <PRICE>10.90</PRICE>
+        <YEAR>1985</YEAR>
+      </CD>
+      <CD>
+        <TITLE>Hide your heart</TITLE>
+        <ARTIST>Bonnie Tyler</ARTIST>
+        <COUNTRY>UK</COUNTRY>
+        <COMPANY>CBS Records</COMPANY>
+        <PRICE>9.90</PRICE>
+        <YEAR>1988</YEAR>
+      </CD>
+    </CATALOG>
+  }
 }
